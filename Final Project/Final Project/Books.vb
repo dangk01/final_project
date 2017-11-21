147692162bf091df6f7619bd03274e6c035cc473
Khoa Tan Dang <dangk01@students.ipfw.edu>
2017-11-20 20:43:35 -0500

    create Books class

---
+++ Final Project/Final Project/Books.vb
@@ -0,0 +1,53 @@
+﻿Public Class Books
+    Private mId As Integer
+    Private mTitle As String
+    Private mAuthorName As String
+    Private mPrice As Decimal
+    Private mQuantityInStock As Integer
+
+
+    Public ReadOnly Property id() As Integer
+        Get
+            Return mId
+        End Get
+    End Property
+
+    Public ReadOnly Property title() As String
+        Get
+            Return mTitle
+        End Get
+    End Property
+    Public ReadOnly Property AuthorName() As String
+        Get
+            Return mAuthorName
+        End Get
+    End Property
+
+    Public ReadOnly Property price() As Decimal
+        Get
+            Return mPrice
+        End Get
+    End Property
+
+    Public Property Quantity() As Integer
+        Get
+            Return mQuantityInStock
+        End Get
+        Set(ByVal value As Integer)
+            mQuantityInStock = value
+        End Set
+    End Property
+
+    Public Sub New(ByVal pId As Integer, ByVal pTitle As String,
+                   ByVal pAuthorName As String,
+                   ByVal pPrice As Decimal,
+                   ByVal pQuantityInStock As Integer)
+        mId = pId
+        mTitle = pTitle
+        mAuthorName = pAuthorName
+        mPrice = pPrice
+        mQuantityInStock = pQuantityInStock
+
+    End Sub
+
+End Class
